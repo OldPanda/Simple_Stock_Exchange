becb03388d34fc3e9d73a4a90aa2d25e70167480
OldPanda <zjh0930@gmail.com>
2015-06-28 23:02:34 -0700

    some bug fixed

--- handlers.py
+++ handlers.py
@@ -18,28 +18,41 @@ class PortfolioHandler(tornado.web.RequestHandler):
             session["cash"] = 100000.00
             session["portfolio"] = []
             session["cur_stock"] = None
+            session["err_notice"] = None
+            session["suc_notice"] = None
             self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
         symbol = self.get_argument('lookupKeyword', None)
         if not symbol:
+            # if no query keyword(symbol) is given
+            err_notice = session["err_notice"]
+            suc_notice = session["suc_notice"]
+            session["err_notice"] = None
+            session["suc_notice"] = None
+            self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
             self.render('portfolio.html',
                         stock=session["cur_stock"],
-                        err_notice=None,
-                        suc_notice=None,
+                        err_notice=err_notice,
+                        suc_notice=suc_notice,
                         cash=session["cash"],
                         portfolio=session["portfolio"])
         else:
             session["cur_stock"] = get_stock_info(symbol)
             if "error" in session["cur_stock"]:
-                err_notice = "Code {}: {}".format(
+                # if query failed
+                session["err_notice"] = "Code {}: {}".format(
                     session["cur_stock"]["error"]["code"],
                     session["cur_stock"]["error"]["message"]
                 )
-                suc_notice = None
-                session["cur_stock"] = None
+                session["suc_notice"] = None
+                session["cur_stock"] = None  # then reset current stock to be None
             else:
-                err_notice = None
-                suc_notice = None
+                session["err_notice"] = None
+                session["suc_notice"] = None
 
+            err_notice = session["err_notice"]
+            suc_notice = session["suc_notice"]
+            session["err_notice"] = None
+            session["suc_notice"] = None
             self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
             self.render('portfolio.html',
                         stock=session["cur_stock"],
@@ -57,44 +70,32 @@ class PortfolioHandler(tornado.web.RequestHandler):
 
         if not len(quantity):
             # if user didn't input the quantity
-            err_notice = "Please input the amount of stocks you want to buy/sell. "
-            self.render('portfolio.html',
-                        stock=session["cur_stock"],
-                        err_notice=err_notice,
-                        suc_notice=None,
-                        cash=session["cash"],
-                        portfolio=session["portfolio"])
+            session["err_notice"] = "Please input the amount of stocks you want to buy/sell. "
+            self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
+            self.redirect("/portfolio")
+            return
+        if int(quantity) <= 0:
+            # if quantity is a negative number or zero
+            session["err_notice"] = "Quantity should be larger than 0. "
+            self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
+            self.redirect("/portfolio")
+            return
 
         if buy != None:
             # if buy button is clicked
             res, session, msg = buy_stock(session, int(quantity))
-            err_notice = None
-            suc_notice = None
             if res:
-                self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
-                suc_notice = msg
+                session["suc_notice"] = msg
             else:
-                err_notice = msg
-            self.render('portfolio.html',
-                        stock=session["cur_stock"],
-                        err_notice=err_notice,
-                        suc_notice=suc_notice,
-                        cash=session["cash"],
-                        portfolio=session["portfolio"])
+                session["err_notice"] = msg
+            self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
+            self.redirect("/portfolio")
         elif sell != None:
             # if sell button is clicked
             res, session, msg = sell_stock(session, int(quantity))
-            err_notice = None
-            suc_notice = None
             if res:
-                self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
-                suc_notice = msg
+                session["suc_notice"] = msg
             else:
-                err_notice = msg
-            self.render('portfolio.html',
-                        stock=session["cur_stock"],
-                        err_notice=err_notice,
-                        suc_notice=suc_notice,
-                        cash=session["cash"],
-                        portfolio=session["portfolio"])
-
+                session["err_notice"] = msg
+            self.set_secure_cookie("Benzinga", pickle.dumps(session), expires=time.time()+900)
+            self.redirect("/portfolio")
